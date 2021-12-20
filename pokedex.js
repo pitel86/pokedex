69b50de40262bfcbc9c2e5acc7f272374bc47886
pitel86 <pitelsergio@gmail.com>
2021-12-20 16:39:35 +0100

    Search Completed

--- pokedex.js
+++ pokedex.js
@@ -1,6 +1,9 @@
 const myPokemons = [];
 const pokeUrl = "https://pokeapi.co/api/v2/pokemon/";
-const pokedexDiv$$ = document.querySelector('#pokedex');
+const pokedexDiv$$ = document.querySelector("#pokedex");
+const listDiv$$ = document.querySelector(".listPokemon");
+const myButton$$ = document.querySelector("button");
+const myInput$$ = document.querySelector("input");
 
 const pokedex = async () => {
   for (let i = 1; i <= 151; i++) {
@@ -9,33 +12,53 @@ const pokedex = async () => {
     myPokemons.push(resJson);
   }
 
-  pintarPokemons()
-
+  drawPokemons();
 };
 
-const pintarPokemons = async () => {
-  
-    for (let i = 0; i < myPokemons.length; i++) {
-        const pokemon = myPokemons[i];
-        let pokemonDiv$$ = document.createElement('div');
-        pokemonDiv$$.classList.add('pokemon');
-        pokemonDiv$$.innerHTML = `
+const drawPokemons = async () => {
+  pokedexDiv$$.innerHTML = "";
+  for (let i = 0; i < myPokemons.length; i++) {
+    const pokemon = myPokemons[i];
+    drawPokemon(pokemon, pokedexDiv$$);
+  }
+  // console.log(pokemons)
+};
+
+const drawPokemon = async (pokemon, div$$) => {
+  let pokemonDiv$$ = document.createElement("div");
+  pokemonDiv$$.classList.add("pokemon");
+  pokemonDiv$$.innerHTML = `
           <div class="pokemon__element1">
-            <img class="pokemon__element1--img" src="${pokemon.sprites.other["official-artwork"].front_default}">
+            <img class="pokemon__element1--img" src="${
+              pokemon.sprites.other["official-artwork"].front_default
+            }">
           </div>
           <div class="pokemon__element2">
             <h2 class="pokemon__element2--h2">${pokemon.name}</h2>
-            <h3 class="pokemon__element2--h3">${pokemon.types.map((type) => type.type.name).join(", ")}</h3>
-            <p class="pokemon__element2--p">Peso: ${pokemon.weight/10} Kg</p>
-            <p class="pokemon__element2--p">Altura: ${pokemon.height/10} M</p>
+            <h3 class="pokemon__element2--h3">${pokemon.types
+              .map((type) => type.type.name)
+              .join(", ")}</h3>
+            <p class="pokemon__element2--p">Peso: ${pokemon.weight / 10} Kg</p>
+            <p class="pokemon__element2--p">Altura: ${pokemon.height / 10} M</p>
           </div>
         `;
-        pokedexDiv$$.appendChild(pokemonDiv$$);
-    }
-    // console.log(pokemons)
-}
+  div$$.appendChild(pokemonDiv$$);
+};
+
+myButton$$.addEventListener("click", () => {
+  searchPokemon(myInput$$.value);
+});
+
+const searchPokemon = (name) => {
+  listDiv$$.innerHTML = "";
+  const filteredPokemons = myPokemons.filter((pokemon) =>
+    pokemon.name.includes(name)
+  );
+  for (let i = 0; i < filteredPokemons.length; i++) {
+    const pokemon = filteredPokemons[i];
+    drawPokemon(pokemon, listDiv$$);
+  }
+};
 
 pokedex();
 
-
-//console.log(myPokemons);
